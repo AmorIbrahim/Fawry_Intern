6058c68f0c3a84748c2d32bbf9c65977cc575a36
amoor <amorebrahim58@gmail.com>
2025-07-04 22:34:42 +0300

    Last modify 1

--- src/Task.java
+++ src/Task.java
@@ -3,8 +3,8 @@ import java.time.LocalDate;
 public class Task {
     public static void main(String [] args){
 
-        Product tv = new ShippingServiceProduct("Smart TV", 999.99, 10, 15.5);
-        Product mobile = new NonExpireProduct("Smartphone", 799.99, 20);
+        Product tv = new ShippingServiceProduct("Smart TV", 1000, 10, 15.5);
+        Product mobile = new NonExpireProduct("Smartphone", 850, 20);
         Product cheese = new ShippingServiceProductExpire("Cheese", 100, 50, LocalDate.now().plusDays(7),.2);
         Product scratchCard = new NonExpireProduct("Mobile Scratch Card", 10.00, 100);
         Product biscuits = new ShippingServiceProductExpire("Biscuits", 150, 5,LocalDate.now().plusDays(7),.7);
